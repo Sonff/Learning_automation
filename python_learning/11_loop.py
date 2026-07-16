779f5c52b44176649200ab4f869b59a9d623d577
sonff <sroy52649@gmail.com>
2026-07-16 08:24:58 +0530

    add new file

---
+++ python_learning/11_loop.py
@@ -0,0 +1,18 @@
+# # for i in range (10):
+# #     print ("shayan sarkar")
+
+# # count = 1
+# # while count <=10:
+# #     print("shayan sarkar")
+# #     count = count +1
+
+# x = int(input("enter your age: "))
+# if (x <= 18):
+#     print("you are a child")
+# else:
+#     print("you are adult")
+
+def add(a,b):
+    return a+b
+sum = add(10,20)
+print (sum)
