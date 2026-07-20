4c7c027f0d3c9ca67755c8fbcb3685b96a7e0c49
sonff <sroy52649@gmail.com>
2026-07-20 20:01:06 +0530

    add new file

--- python_learning/12_pattern.py
+++ python_learning/12_pattern.py
@@ -1,9 +1,46 @@
-# row = 5
-# for i in range (row):
-#     for j in range (row):
-#         print("*", end=" ")
-#     print()
+# # row = 5
+# # for i in range (row):
+# #     for j in range (row):
+# #         print("*", end=" ")
+# #     print()
     
-row = 5
-for i in range (1, row+1 ):
-    print ("*" * i)
+# # * * * * * 
+# # * * * * * 
+# # * * * * * 
+# # * * * * * 
+# # * * * * * 
+
+# # row = 5
+# # for i in range (1, row+1 ):
+# #     print ("*" * i)
+
+# # *
+# # **
+# # ***
+# # ****
+# # *****
+
+# row = 5
+# for i in range (row, 0, -1):
+#     print ("*" * i)
+
+# # *****
+# # ****
+# # ***
+# # **
+# # *
+
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+
+    # Print spaces
+    for j in range(rows - i):
+        print(" ", end="")
+
+    # Print stars
+    for k in range(2 * i - 1):
+        print("*", end="")
+
+    print()
